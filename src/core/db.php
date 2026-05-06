8d20ef1e112514e1e0a87298d2628e9d5ce90dd3
AsqerizaDevelopment <asqerizadevelopmet@gmail.com>
2026-05-06 13:05:14 +0200

    feat: db connection

--- src/core/db.php
+++ src/core/db.php
@@ -1,2 +1,13 @@
-{# -- Database connection settings #}
+{# Include database config #}
+<?php
+include_once 'config.php';
+// Create PDO connection
 
+try {
+    $dsn = "mysql:host={$config['db']['host']};dbname={$config['db']['database']};charset=utf8mb4";
+    $pdo = new PDO($dsn, $config['db']['username'], $config['db']['password']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Database connection successful!";
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
--- src/data/config.php
+++ src/data/config.php
@@ -1,1 +1,9 @@
 {# -- database config #}
+<?php
+
+$config['db'] = [
+    'host' => '217.154.164.56',
+    'username' => 'u28_tGPyQfy6a2',
+    'password' => 'D03SNQnF+wUgG3I@f9d+@^dY',
+    'database' => 's28_StreamHiveTijn',
+];
